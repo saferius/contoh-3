3efc728bb15c5513ce70c533284929add5a91637
saferius <103491341+saferius@users.noreply.github.com>
2022-04-12 16:02:46 +0700

    Add files via upload

---
+++ View-Latihan.php
@@ -0,0 +1,13 @@
+<html>
+    <head>
+        <title>
+            Latihan 1
+        </title>
+    </head>
+    <body>
+        Hallo kawan.. yuk kita belajar Programming..!!!<br>
+        Nilai 1 = <?= $nilai1; ?>
+        Nilai 2 = <?= $nilai2; ?>
+        ini hasil dari pemodelan dengan methide penjumlahan  yaitu <?=$nilai1. "+" .$nilai2. "=" . $hasil; ?>
+    </body>
+</html>
